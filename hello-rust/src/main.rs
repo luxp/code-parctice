ac7955ca51afb7ec54fa41e1b465c9157630399f
luxp <luxianpo@gmail.com>
2019-10-26 22:35:31 +0800

    rust-test

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,10 +1,35 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
